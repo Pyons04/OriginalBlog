248b9162c1f4c0a93a313db29100eb7ac4ab0f09
COMPUTERNAME\user <broad.basic15@gmail.com>
2018-06-09 22:00:02 +1000

    implement markdown rendring engine

--- main.rb
+++ main.rb
@@ -1,19 +1,31 @@
 require 'sinatra'
 require 'pg'
+require 'redcarpet'
+
+
+markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, autolink: true, tables: true)
 
 
 get '/' do
    connection = PG::connect(:host => "localhost", :user => "postgres", :password => "takahama0613", :dbname => "blog",:port=>"5432")
    result = connection.exec("SELECT * FROM blogs")
+
      # データベースへのコネクションを切断する
      connection.finish
 
      array = []
+     dates = []
      #データベースの内容を配列に収納
      result.each do |record|
      array<<record['content']
+     dates<<record['post_date']
      end
 
-     p array
-  'hello'
+     put = array[1]
+     date = dates[1]
+
+
+     html = markdown.render(put)
+
+  "#{date}#{html}"
 end
