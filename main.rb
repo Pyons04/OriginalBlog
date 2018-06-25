23c1b86d5f06ae995c689392725cdfb4977d9729
COMPUTERNAME\user <broad.basic15@gmail.com>
2018-06-25 14:42:04 +1000

    add CSS files

--- main.rb
+++ main.rb
@@ -15,27 +15,47 @@ get '/' do
 
      array = []
      dates = []
+     titles = []
+     #データベースの内容を配列に収納
+     result.each do |record|
+     array<<record['content']
+     dates<<record['post_date']
+     titles<<record['title']
+     end
+
+     put = array[0]
+     @html = markdown.render(put)
+     @title = titles[0]
+     @date = dates[0]
+
+    erb :home
+end
+
+
+get '/oldpost/:number' do
+
+   number = params[:number].to_i
+   
+   connection = PG::connect(:host => "localhost", :user => "postgres", :password => "takahama0613", :dbname => "blog",:port=>"5432")
+   result = connection.exec("SELECT * FROM blogs")
+
+     # データベースへのコネクションを切断する
+     connection.finish
+
+     array = []
+     dates = []
      #データベースの内容を配列に収納
      result.each do |record|
      array<<record['content']
      dates<<record['post_date']
      end
 
-     put = array[0]
-     date = dates[0]
-
+     put = array[number]
+     date = dates[number]
 
      html = markdown.render(put)
 
   "#{date}#{html}"
-end
-
-
-get '/oldpost/:number' do
-
-   number = params[:number]
-   
-   "#{date}#{html}"
 end
 
 
