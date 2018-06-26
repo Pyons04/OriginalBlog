a40ecd2bd6101b5087930812cffcd617334f6c67
COMPUTERNAME\user <broad.basic15@gmail.com>
2018-06-26 16:57:57 +1000

    add links to next pages

--- main.rb
+++ main.rb
@@ -56,6 +56,7 @@ get '/' do
 
     unless fourth_blog.nil? then
         @backnumber_link = true
+        @link = "/oldpost/1"
     end
 
     erb :home
@@ -64,28 +65,65 @@ end
 
 get '/oldpost/:number' do
 
-   number = params[:number].to_i
-   
-   connection = PG::connect(:host => "localhost", :user => "postgres", :password => "takahama0613", :dbname => "blog",:port=>"5432")
+    connection = PG::connect(:host => "localhost", :user => "postgres", :password => "takahama0613", :dbname => "blog",:port=>"5432")
    result = connection.exec("SELECT * FROM blogs")
 
      # データベースへのコネクションを切断する
      connection.finish
 
-     array = []
-     dates = []
-     #データベースの内容を配列に収納
+     ids = []
      result.each do |record|
-     array<<record['content']
-     dates<<record['post_date']
+     ids<<record['id']
      end
 
-     put = array[number]
-     date = dates[number]
 
-     html = markdown.render(put)
+     param = params[:number].to_i*3
+     latest_id = ids.max.to_i - param  #最新の記事のidから3つ前のidの記事を持ってくる,2ページ目以降はパラメータの値*3つ分古い記事を持ってくる。
 
-  "#{date}#{html}"
+  unless latest_id.nil? then
+     latest_blog = result.select{|records| records['id'] == latest_id.to_s}.first
+
+     @html = markdown.render(latest_blog['content'])
+     @title = latest_blog['title']
+     @date = latest_blog['post_date']
+     @time = latest_blog['post_time']
+
+     second_id = latest_id - 1 #なぜかlatest_idがstringになってしまっているため、Integerに戻さないと計算できない。（バグ）
+     second_blog = result.select{|records| records['id'] == second_id.to_s}.first
+  end
+
+   unless second_blog.nil? then
+     @html2 = markdown.render(second_blog['content'])
+     @title2 = second_blog['title']
+     @date2 = second_blog['post_date']
+     @time2 = second_blog['post_time']
+   end
+
+
+     third_id = latest_id - 2
+     third_blog = result.select{|records| records['id'] == third_id.to_s}.first
+
+   unless second_blog.nil? then
+     @html3 = markdown.render(third_blog['content'])
+     @title3 = third_blog['title']
+     @date3 = third_blog['post_date']
+     @time3 = third_blog['post_time']
+   end
+
+   fourth_id = latest_id - 3
+   fourth_blog = result.select{|records| records['id'] == fourth_id.to_s}.first
+
+    @backnumber_link = false
+
+    unless fourth_blog.nil? then
+        @backnumber_link = true
+        link = params[:number].to_i+1
+        @link = "/oldpost/#{link}"
+    end
+
+    erb :home
+
+
 end
 
 
